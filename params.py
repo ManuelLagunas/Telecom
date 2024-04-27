0a941f4dec3682f162d2b0ed5f54cc6caa5642ad
Manuel Lagunas <147188990+ManuelLagunas@users.noreply.github.com>
2024-04-26 21:32:26 -0600

    Archivo de parametros modificado

--- params.py
+++ params.py
@@ -1,17 +1,11 @@
-# Librerias ---------------------------------------- 
+# Libreries ---------------------------------------- 
 
 import platform 
-from functions.crear_formatos_fecha import *
 
-# Systema operativo ---------------------------------------- 
+# Operative sistem ---------------------------------------- 
 
 sistema_operativo = platform.system()
 
-# Entrenamiento o ejecucion ---------------------------------------- 
+# Training or ejecution ---------------------------------------- 
 
 bool_entrtenamiento = False
-
-# Fecha por defecto ---------------------------------------- 
-
-periodo_YYYYMM_por_defecto = "202311"
-periodo_YYYYMM, anio_mes_dia_formato_ini_date, anio_mes_dia_formato_ini, anio_mes_dia_formato_fin = crear_formatos_fecha(periodo_YYYYMM_por_defecto)
--- pipeline/p01_pipeline_entrenamiento.py
+++ pipeline/p01_pipeline_entrenamiento.py
@@ -1,19 +1,9 @@
-# Librerias ----------------------------------------
+# Libraries ----------------------------------------
 
 import os, sys
 import argparse
 sys.path.append(os.getcwd()) # Esto es para agregar al path la ruta de ejecución actual y poder importar respecto a la ruta del proyecto, desde donde se debe ejecutar el código
 import params as params
-
-# Argumentos por linea de comandos ---------------------------------------- 
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--periodo', default=f'{params.periodo_YYYYMM}', help='periodo en formato YYYYMM')
-
-try:
-    args = parser.parse_args()
-except argparse.ArgumentTypeError as e:
-    print(f"Invalid argument: {e}")
     
 # Definir extension de ejecutables ---------------------------------------- 
 
